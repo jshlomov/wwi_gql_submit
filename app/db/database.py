be9fe06e2ebeda76880f17ea191f661a85eb4e4b
jshlomov <jonatanshv@gmail.com>
2024-10-31 10:17:00 +0200

    change base to init

--- app/db/database.py
+++ app/db/database.py
@@ -6,7 +6,6 @@ from app.settings.config import DB_URL
 engine = create_engine(DB_URL)
 session_maker = sessionmaker(bind=engine)
 
-Base = declarative_base()
 
 class Test(Base):
     __tablename__ = "test"
